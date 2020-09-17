f3c61cfb6c6e8e637b5d8fa99e6b2b0041588985
Marcelo11114 <dedosearte@hotmail.com>
2020-09-17 06:24:34 -0300

    Meme eu to nervoso

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MemeTonervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Pensa;
+            Console.WriteLine("Oi, O que vc pensa de mim?");
+            Pensa = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Quer saber o que penso de voce?");
+            Console.WriteLine($"{Pensa.Remove(10)}....Não, Pera \n Eu to nervoso");
+            Console.ReadKey();
+            
+        }  
+
+    }
+}
